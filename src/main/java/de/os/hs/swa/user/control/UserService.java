62555cbee65dff37d5b4676ac0b79a8f4f7a288f
Laura Peter <laura.peter@hs-osnabrueck.de>
2021-07-30 17:13:31 +0200

    getcreator namein quizressource

--- src/main/java/de/os/hs/swa/user/control/UserService.java
+++ src/main/java/de/os/hs/swa/user/control/UserService.java
@@ -1,5 +1,6 @@
 package de.os.hs.swa.user.control;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.core.Response;
@@ -8,6 +9,7 @@ import de.os.hs.swa.quiz.acl.UserAdapter;
 import io.quarkus.security.ForbiddenException;
 import io.quarkus.security.identity.SecurityIdentity;
 
+@RequestScoped
 public class UserService implements UserAdapter{
 
     @Inject
@@ -17,7 +19,6 @@ public class UserService implements UserAdapter{
     public boolean isAuthorizedToEdit(String quizUsername){
         String username = identity.getPrincipal().getName();
         if(quizUsername.equals("")){
-            //TODO: add response object in exception param?
             Response response = Response.status(Response.Status.UNAUTHORIZED).entity("Invalid credentials or session").build();
             throw new NotAuthorizedException(response);
         }
