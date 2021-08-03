12385b57e77310b975a7f3873f8c00c766507a37
Laura Peter <laura.peter@hs-osnabrueck.de>
2021-08-03 17:30:47 +0200

    update quiz ressource test & neue werte in import.sql

--- src/main/java/de/os/hs/swa/quiz/entity/Quiz.java
+++ src/main/java/de/os/hs/swa/quiz/entity/Quiz.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -20,7 +21,7 @@ import de.os.hs.swa.category.entity.Category;
 
 @Entity
 public class Quiz {
-    @Id @GeneratedValue 
+    @Id @GeneratedValue(strategy = GenerationType.TABLE)
     @Column(name = "quiz_id")
     private Long id;
 
